75f292133dfb215bfbf549aeb15f80a576d84dd7
Carlos R. Arias <arias@spu.edu>
2020-04-06 20:16:07 -0700

    Almost done
    Implemented most methods, Read needs to be done, with validation. Not tested yet either.

---
+++ vector.cpp
@@ -0,0 +1,165 @@
+//
+// Created by Arias Arevalo, Carlos on 4/6/20.
+//
+
+#include "vector.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using std::ostream;
+using std::istream;
+using std::string;
+using std::stringstream;
+
+Vector::Vector(size_t dimensions) {
+    _size = dimensions;
+    if (dimensions == 0){
+        _data = nullptr;
+    }else{
+        _data = new double[_size];
+        for (size_t i = 0; i < _size; i++) {
+            _data[i] = 0.0;
+        }
+    }
+}
+
+Vector::Vector(const Vector &other) {
+    _size = other._size;
+    if (_size == 0){
+        _data = nullptr;
+    }else{
+        _data = new double[_size];
+        for (size_t i = 0; i < _size; ++i) {
+            _data[i] = other._data[i];
+        }
+    }
+}
+
+Vector::~Vector() {
+    if (_data != nullptr)
+        delete[] _data;
+}
+
+Vector &Vector::operator=(const Vector &rhs) {
+    // Check if it is a self assignment
+    if (&rhs == this)
+        return *this;
+    // Check if they don't have the same size
+    if (_size != rhs._size){
+        if (_data != nullptr)
+            delete[] _data;
+        _size = rhs._size;
+        _data = new double[_size];
+    }
+    for (size_t i = 0; i < _size; ++i) {
+        _data[i] = rhs._data[i];
+    }
+    return *this;
+}
+
+Vector::Vector(size_t dimensions, const double *data) {
+    _size == dimensions;
+    if (_size == 0){
+        _data = nullptr;
+    }else{
+        _data = new double[_size];
+        for (size_t i = 0; i < _size; i++)
+            _data[i] = data[i];
+    }
+}
+
+const double &Vector::operator[](size_t index) const {
+    return _data[index];
+}
+
+double &Vector::operator[](size_t index) {
+    return _data[index];
+}
+
+string Vector::ToString() const {
+    stringstream ss;
+    ss << "[";
+    for (int i = 0; i < _size; ++i) {
+        if (i != _size - 1){
+            ss << _data[i] << ", ";
+        }else{
+            ss << _data[i];
+        }
+    }
+    ss << "]";
+    return ss.str();
+}
+
+double Vector::EuclidianDistance(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return -1; // Impossible distance!
+
+    double sum = 0.0;
+    for (int i = 0; i < _size; ++i) {
+        sum += pow(_data[i] - rhs._data[i], 2.0);
+    }
+    return sqrt(sum);
+}
+
+bool Vector::operator==(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return false;
+    for (int i = 0; i < _size; ++i) {
+        if (_data[i] != rhs._data[i])
+            return false;
+    }
+    return true;
+}
+
+bool Vector::operator!=(const Vector &rhs) const {
+    return !(*this == rhs);
+}
+
+Vector Vector::operator+(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return Vector(0);
+    Vector retVal(_size);
+    for (size_t i = 0; i < _size; ++i) {
+        retVal._data[i] = _data[i] + rhs._data[i];
+    }
+    return retVal;
+}
+
+Vector Vector::operator-(const Vector &rhs) const {
+    if (_size != rhs._size)
+        return Vector(0);
+    Vector retVal(_size);
+    for (size_t i = 0; i < _size; ++i) {
+        retVal._data[i] = _data[i] - rhs._data[i];
+    }
+    return retVal;
+}
+
+bool Vector::IsValid() const {
+    return _size == 0;
+}
+
+size_t Vector::Dimensions() const {
+    return _size;
+}
+
+ostream &Vector::Write(ostream &output) const {
+    output << _size << " ";
+    for (int i = 0; i < _size; ++i) {
+        if (i != _size - 1){
+            output << _data[i] << " ";
+        }else{
+            output << _data[i];
+        }
+    }
+    return output;
+}
+
+istream &Vector::Read(istream &input) {
+    size_t dimension;
+    
+}
+
